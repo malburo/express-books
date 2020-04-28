654adc156df319f875794b8872ece290e6b4898b
malburo <tongquocbao0605@gmail.com>
2020-04-28 09:16:48 +0700

    error handling

--- api/controllers/transaction.controller.js
+++ api/controllers/transaction.controller.js
@@ -1,7 +1,27 @@
 
 const Transaction = require("../../models/transaction.model")
 
-module.exports.index = async (req, res) => {
+module.exports.getAll = async (req, res) => {
   let transactions = await Transaction.find()
   res.json(transactions)
+}
+module.exports.getOne = async (req, res) => {
+    let transaction = await Transaction.findById(req.params.id)
+    res.json(transaction)
+  }
+module.exports.postCreate = async (req, res) => {
+    let transactions = await Transaction.create(req.body);
+    res.json(transactions)
+}
+module.exports.putUpdate = async (req, res) => {
+    let transactions = await Transaction.findByIdAndUpdate(req.params.id, req.body);
+    res.json(transactions)
+}
+module.exports.patchUpdateOne = async (req, res) => {
+    let transactions = await Transaction.findByIdAndUpdate(req.params.id, req.body);
+    res.json(transactions)
+}
+module.exports.deleteId = async (req, res) => {
+    let transactions = await Transaction.findByIdAndRemove(req.params.id);
+    res.json(transactions)
 }
--- api/routes/transaction.route.js
+++ api/routes/transaction.route.js
@@ -3,6 +3,10 @@ var router = express.Router();
 
 const controller = require("../controllers/transaction.controller");
 
-router.get("/", controller.index);
-
+router.get("/", controller.getAll);
+router.post("/", controller.postCreate);
+router.get("/:id", controller.getOne);
+router.put("/:id", controller.putUpdate)
+router.patch("/:id", controller.patchUpdateOne)
+router.delete("/:id", controller.deleteId);
 module.exports = router;
