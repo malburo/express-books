90e88f8b9c9e9411ab31453802a47b9246224f3a
malburo <tongquocbao0605@gmail.com>
2020-05-09 18:29:06 +0700

    Feature/search-books-by-title

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -1,10 +1,10 @@
-const Book = require("../models/book.model")
+const Book = require("../models/book.model");
 let cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
   cloud_name: process.env.cloud_name,
   api_key: process.env.api_key,
-  api_secret: process.env.api_secret
+  api_secret: process.env.api_secret,
 });
 
 module.exports.index = async (req, res, next) => {
@@ -12,8 +12,8 @@ module.exports.index = async (req, res, next) => {
   let perPage = 8;
   let start = (page - 1) * perPage;
   let end = page * perPage;
-  let books = await Book.find().sort({_id: -1})
-  let bookFilter = books.slice(start, end)
+  let books = await Book.find().sort({ _id: -1 });
+  let bookFilter = books.slice(start, end);
   let isEmtyBooks = false;
   if (bookFilter.length === 0) {
     isEmtyBooks = true;
@@ -21,6 +21,16 @@ module.exports.index = async (req, res, next) => {
   res.render("books/index", {
     books: bookFilter,
     page: page,
-    isEmtyBooks: isEmtyBooks
+    isEmtyBooks: isEmtyBooks,
+  });
+};
+
+module.exports.search = async (req, res, next) => {
+  let books = await Book.find().sort({ _id: -1 });
+  let booksSearch = books.filter((book) => {
+    return book.title.toLowerCase().indexOf(req.query.title.toLowerCase()) >= 0;
+  });
+  res.render("books/index", {
+    books: booksSearch,
   });
 };
--- routes/book.route.js
+++ routes/book.route.js
@@ -4,5 +4,5 @@ var router = express.Router();
 const controller = require("../controllers/book.controller");
 
 router.get("/", controller.index);
-
+router.get("/search", controller.search);
 module.exports = router;
